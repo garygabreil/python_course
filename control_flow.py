b89c9a1649170819521e7b4a0133d0e0be5cd38a
Gary Gabreil <garygabreil@Garys-MacBook-Air.local>
2025-05-17 11:42:10 +0530

    day-8

---
+++ control_flow.py
@@ -0,0 +1,62 @@
+# keyword - if, elif, else
+# if else
+# nested if 
+
+
+
+# temperature = 30
+# if temperature >= 30:
+#     print("It will be hot today")
+# else:
+#     print("It's summer time")
+
+# age = 6
+# if age > 18 or age <= 49:
+#     print("this person is a Major")
+# elif age > 50:
+#     print("this person is a veteran")
+# elif age > 5:
+#     print("this person is a minor")
+# else:
+#     print("this person has not age defined")
+
+color = "blue"
+if (color == "red" or color == "blue"):
+    print("this is red & blue")
+elif color =="green":
+    print("this is green")
+elif color =="yellow":
+    print("this is yellow")
+else:
+    print("this is default case")
+
+
+##Loop control - statment
+# for
+# while
+
+#syntax
+#for i in range(10)
+
+#array
+fruits=["apple","orange","cherry","banana"]
+for temp_name in fruits:
+    print(temp_name + "s")
+
+
+
+# banks= ["rbs", "natwest", "ultser"]
+
+# for bank_brand in banks:
+#     if("rbs"):
+#         datasource
+#         username
+#         password
+#     elif("natwest"):
+#         datasource
+#         username
+#         password
+#     elif("ultser"):
+#          datasource
+#         username:
+#         password
